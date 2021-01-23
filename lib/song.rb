8666b421fe2640728e72680c1ed7c3c64c63d27e
Fay Vera <fvera.cavalcante@gmail.com>
2021-01-22 23:02:58 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,47 @@ class Song
   @@all = []
 
   def self.all
-    @@all
+    @@all << name
   end
 
   def save
     self.class.all << self
   end
+  # given
+
+  def self.create
+    s = self.new
+    s.save
+    s
+  end 
+
+  def self.new_by_name(name)
+    s = self.new
+    s.name = name
+    s
+  end
+
+  def self.create_by_name(name)
+    s = self.new
+    s.name = name 
+    s.save
+    s
+  end
+
+  def self.find_by_name(song_name)
+    @@all.find do |song|
+    song.name == song_name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name
+      self.find_by_name(name)
+    else     
+      self.create_by_name(name)
+  end
+end
+
 
 end
+ 
